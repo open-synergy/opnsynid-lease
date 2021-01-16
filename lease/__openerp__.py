ac0513bc1004f8f8f0d0ba91ed1a21b6f90f2a47
Andhitia Rama <andhitia.r@gmail.com>
2021-01-17 02:13:04 +0700

    [lease] 8.0.1.0.0

---
+++ lease/__openerp__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Lease Management",
+    "version": "8.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
---
+++ lease/models/res_config.py
@@ -0,0 +1,20 @@
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
+
+
+class ResConfig(models.TransientModel):
+    _name = "loan.config_setting"
+    _inherit = "res.config.settings"
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
+
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        default=lambda self: self._default_company_id(),
+    )
